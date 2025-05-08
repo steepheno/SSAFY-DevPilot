f2b492385dad3a7cebeb1a6b0e8cfa6560bd334b
한재서 <hanahyun1@naver.com>
2025-05-08 16:12:50 +0900

    refactor: github 등록 로직 수정[한재서]

--- Backend/scripts/register_github_job.sh
+++ Backend/scripts/register_github_job.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# GitHub 파이프라인 등록 스크립트
-source ./utils.sh
-source ./ssh_connection.sh
-source ./jenkins_configuration.sh
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Github 파이프라인 등록 스크립트
+source "$SCRIPT_DIR/utils.sh"
+source "$SCRIPT_DIR/ssh_connection.sh"
+source "$SCRIPT_DIR/jenkins_configuration.sh"
 
 # ========================================================
 # 오브젝트 선언
@@ -18,10 +20,6 @@ declare -A SERVER=(
 # 인자 파싱
 for arg in "$@"; do
   case $arg in
-    --pem-path=*) SERVER[pem_path]="${arg#*=}" ;;
-    --ec2-host=*) SERVER[host]="${arg#*=}" ;;
-    --jenkins-port=*) SERVER[jenkins_port]="${arg#*=}" ;;
-    --jenkins-password=*) JENKINS_PASSWORD="${arg#*=}" ;;
     --git-token=*) GIT_TOKEN="${arg#*=}" ;;
     --git-credentials-id=*) GIT_CREDENTIALS_ID="${arg#*=}" ;;
     --git-repo-url=*) GIT_REPO_URL="${arg#*=}" ;;
@@ -40,6 +38,6 @@ export JENKINS_PASSWORD
 # 1. GitHub 자격 증명 등록
 register_git_credentials "github" "$GIT_TOKEN" "$GIT_CREDENTIALS_ID"
 
-# 2. Jenkins Job XML 생성 및 등록
-generate_job_config "$GIT_REPO_URL" "$GIT_BRANCH" "$GIT_CREDENTIALS_ID" "github"
+# 2. Jenkins 멀티브랜치 Job XML 생성 및 등록
+generate_job_config "$GIT_REPO_URL" "$GIT_CREDENTIALS_ID" "gitlab" "$JENKINS_JOB_NAME"
 create_job_in_jenkins "$JENKINS_JOB_NAME"
