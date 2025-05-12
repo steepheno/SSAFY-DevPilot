f32e6ca9a46af74afc07d5f9ac0737eb73c6d361
유준선 <rjs7289@naver.com>
2025-05-12 12:18:15 +0900

    feat: 빌드 기록 페이지 생성 UI[유준선]

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { MainPage, NewBuildPage, DockerSettings, ConfigurePage } from '@/pages/';
 import JenkinsSettings from '@/features/jenkins-settings/ui/JenkinsSettings';
+import PageLayout from '@/widgets/PageLayout';
+import BuildList from '@/pages/buildLog/ui/BuildList';
 
 const Router = createBrowserRouter([
   {
@@ -26,6 +28,15 @@ const Router = createBrowserRouter([
         element: <ConfigurePage />,
       },
     ],
+  },
+  {
+    path: '/builds',
+    handle: { breadcrumb: '빌드 기록' },
+    element: (
+      <PageLayout>
+        <BuildList />
+      </PageLayout>
+    ),
   },
 ]);
 
