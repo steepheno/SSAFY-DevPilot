915cb7ab465a868c20a4ee0fa51b11f5f88453ff
유준선 <rjs7289@naver.com>
2025-05-12 15:32:13 +0900

    refactor: 상세 내용 입력 버튼, textarea 입력 부분 컴포넌트화
    - MainPage.tsx와 BuildDetail.tsx에서 공통으로 쓰이는 버튼과 입력 부분
      컴포넌트화 진행
    
    S12P31A301-120

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,5 +1,6 @@
+import DetailButton from '@/shared/ui/DetailButton';
+import DetailInput from '@/shared/ui/DetailInput';
 import PageLayout from '@/widgets/PageLayout';
-import { Pencil } from 'lucide-react';
 import { useState } from 'react';
 
 const MainPage = () => {
@@ -22,40 +23,22 @@ const MainPage = () => {
             </div>
           )}
           <div className="flex justify-end">
-            <button
-              className="flex items-center rounded-lg bg-gray-100 px-3 py-2"
-              onClick={() => setIsEditMode(true)}
-            >
-              <Pencil />
-              <span className="ml-3">{savedText ? '내용 수정' : '상세 내용 입력'}</span>
-            </button>
+            <DetailButton
+              isEditMode={isEditMode}
+              setIsEditMode={setIsEditMode}
+              savedText={!!savedText}
+            />
           </div>
         </div>
       ) : (
         <div className="my-10 px-5">
-          <textarea
-            className="w-[950px] rounded-lg border border-gray-300 p-3 focus:border-blue-500 focus:outline-none"
-            rows={4}
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="상세 내용을 입력하세요"
-            autoFocus
+          <DetailInput
+            isEditMode={isEditMode}
+            setIsEditMode={setIsEditMode}
+            text={text}
+            setText={setText}
+            onSubmit={submitTitle}
           />
-          <div className="mt-2 flex justify-end">
-            <button
-              onClick={() => setIsEditMode(false)}
-              className="flex items-center justify-center rounded-lg bg-gray-200 px-3 py-2"
-            >
-              <span>취소</span>
-            </button>
-            <button
-              onClick={submitTitle}
-              className="ml-3 flex items-center justify-center rounded-lg bg-blue-500 px-3 py-2 text-white"
-            >
-              {/* <Check /> */}
-              <span>저장</span>
-            </button>
-          </div>
         </div>
       )}
       <table className="w-full border-separate border-spacing-0 px-5">
