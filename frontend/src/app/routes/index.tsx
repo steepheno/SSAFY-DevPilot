705fa9f3e711ed62dc130d07f0c7be49e296e7bd
유준선 <rjs7289@naver.com>
2025-05-12 15:33:51 +0900

    feat: 빌드 상세 페이지 UI 구현중
    - 피그마의 2-3 빌드 상세 페이지 구현중
    - index.tsx의 라우팅 사이드바에 추가함
    
    S12P31A301-120

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -1,8 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Outlet } from 'react-router-dom';
 import { MainPage, NewBuildPage, DockerSettings, ConfigurePage } from '@/pages/';
 import JenkinsSettings from '@/features/jenkins-settings/ui/JenkinsSettings';
 import PageLayout from '@/widgets/PageLayout';
 import BuildList from '@/pages/buildLog/ui/BuildList';
+import BuildDetail from '@/pages/buildLog/ui/BuildDetail';
 
 const Router = createBrowserRouter([
   {
@@ -34,9 +35,21 @@ const Router = createBrowserRouter([
     handle: { breadcrumb: '빌드 기록' },
     element: (
       <PageLayout>
-        <BuildList />
+        <Outlet />
       </PageLayout>
     ),
+    children: [
+      {
+        path: '',
+        handle: { breadcrumb: '빌드 목록' },
+        element: <BuildList />,
+      },
+      {
+        path: 'detail',
+        handle: { breadcrumb: '빌드 상세' },
+        element: <BuildDetail />,
+      },
+    ],
   },
 ]);
 
