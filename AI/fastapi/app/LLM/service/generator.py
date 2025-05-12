afa9527c051c40dfeb34f53ee3d6315f5efffbe7
sungmoon <behuman1023@naver.com>
2025-05-12 17:41:19 +0900

    refactor: 시스템 프롬프트 CoT 적용용

--- AI/fastapi/app/LLM/service/generator.py
+++ AI/fastapi/app/LLM/service/generator.py
@@ -10,7 +10,9 @@ import torch
 from langsmith import traceable
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
-model = AutoModelForCausalLM.from_pretrained(MODEL_ID, device_map="auto", torch_dtype=torch.bfloat16 )
+model = AutoModelForCausalLM.from_pretrained(MODEL_ID, device_map="auto", torch_dtype=torch.bfloat16,  offload_folder="./offload", )
+
+
 generator_pipeline = pipeline(
     "text-generation",
     model=model,
@@ -26,12 +28,14 @@ llm = HuggingFacePipeline(pipeline=generator_pipeline)
 embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_ID)
 
 template = """
-당신은 CICD와 Jenkins에 특화된 전문가입니다. 아래 문서를 바탕으로 질문에 한국어로 간결하고 정확하게 답변해 주세요.
+당신은 CICD와 Jenkins에 특화된 전문가입니다. 아래 문서를 바탕으로 질문에 정확하게 답변해 주세요.
 
 ※ 조건
-- 답변 외의 문장은 쓰지 마세요.
-- CONTEXT의 내용을 복사하지 말고, 필요한 정보만 활용해 풀어서 설명하세요.
-- CICD 및 Jenkins 용어는 쉽게 설명하세요.
+- 문서에 포함된 정보를 최대한 활용하세요.
+- 문서를 그대로 복사하지 말고, 문서 내용을 기반으로 풀어서 설명하세요.
+- 답변하는 과정에서, 문서의 문장을 활용해 논리적으로 추론하는 과정을 거치세요.
+- CICD나 젠킨스와 관련없는 질문은 '해당 내용은 답변할 수 없습니다'라고 하세요.
+- 영어로 생각하는 추론 과정을 거친 후, 반드시 한글로 답변하세요.
 
 [문서]
 {context}
@@ -41,12 +45,15 @@ template = """
 
 [답변]
 """
+
 prompt = PromptTemplate.from_template(template)
 chain = prompt | llm | StrOutputParser()
 
 # 4. 서비스 함수
-@traceable(name="generate_answer")
 def generate_answer(question: str) -> str:
     docs = query_multiple_indexes(query=question, embedding_model=embeddings)
     context = " ".join([doc['metadata']['text'] for doc in docs])
     return chain.invoke({"context": context, "question": question})
+
+def generate_answer_NoRAG(question: str) -> str:
+    return chain.invoke({"context": '', "question": question})
