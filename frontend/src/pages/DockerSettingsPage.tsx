40a82c24627887177207a898f75f2adcf91bc056
isemae <deleted.lee13@gmail.com>
2025-05-13 16:16:24 +0900

    chore: 미사용 함수 호출 주석처리[이지운]

--- frontend/src/pages/DockerSettingsPage.tsx
+++ frontend/src/pages/DockerSettingsPage.tsx
@@ -1,18 +1,17 @@
-import { useState } from 'react';
 import { useFormStore } from '@/shared/store';
 import BuildInfo from '@/features/dockerfile-settings/ui/BuildInfo';
 import { checkInpuValidation } from '@/features/dockerfile-settings/lib/checkInputValidation';
 import ProjectEnvironment from '@/features/dockerfile-settings/ui/ProjectEnvironment';
 import MySqlInfo from '@/features/dockerfile-settings/ui/MySqlInfo';
-import DockerfileConfig from '@/entities/dockerFile/types';
-import { generateDockerfile } from '@/entities/dockerFile/api';
-import { useNavigate } from 'react-router-dom';
+// import DockerfileConfig from '@/entities/dockerFile/types';
+// import { generateDockerfile } from '@/entities/dockerFile/api';
+// import { useNavigate } from 'react-router-dom';
 
 const DockerfileSettings = () => {
   // Navigate
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const { projectConfig, frontendConfig, backendConfig, databaseConfig } = useFormStore();
+  const { frontendConfig, backendConfig } = useFormStore();
 
   // 프로젝트 환경 체크박스 상태 관리
   // const projectEnvironment = useProjectEnvironment();
@@ -23,48 +22,47 @@ const DockerfileSettings = () => {
   // const [isLoading, setIsLoading] = useState(false);
 
   // 응답 결과 상태 관리
-  const [, setResult] = useState(null);
 
-  const buildDockerfile = async () => {
-    // 빌드 정보 검증
-    // if (!buildInfo.validateBuildInfo()) {
-    //   return;
-    // }
-
-    // MySQL 설정 검증
-    // if (!mySqlInfo.validateMySqlInfo()) {
-    //   return;
-    // }
-
-    // API 요청 데이터 구성
-    const dockerConfig: DockerfileConfig = {
-      ...backendConfig,
-      ...frontendConfig,
-      ...projectConfig,
-      ...databaseConfig,
-    };
-
-    console.log('API 요청 데이터: ', dockerConfig);
-
-    try {
-      // setIsLoading(true);
-
-      // API 함수 호출
-      const data = await generateDockerfile(dockerConfig);
-      console.log('호출 직후 data: ', data);
-
-      // 응답 결과 설정
-      // setResult(data);
-      console.log('setResult 직후 data: ', data);
-
-      navigate('/new/configure');
-    } catch (error) {
-      alert('Dockerfile 생성 중 오류 발생');
-      console.error('오류 메시지: ', error);
-    } finally {
-      // setIsLoading(false);
-    }
-  };
+  // const buildDockerfile = async () => {
+  //   // 빌드 정보 검증
+  //   // if (!buildInfo.validateBuildInfo()) {
+  //   //   return;
+  //   // }
+  //
+  //   // MySQL 설정 검증
+  //   // if (!mySqlInfo.validateMySqlInfo()) {
+  //   //   return;
+  //   // }
+  //
+  //   // API 요청 데이터 구성
+  //   const dockerConfig: DockerfileConfig = {
+  //     ...backendConfig,
+  //     ...frontendConfig,
+  //     ...projectConfig,
+  //     ...databaseConfig,
+  //   };
+  //
+  //   console.log('API 요청 데이터: ', dockerConfig);
+  //
+  //   try {
+  //     // setIsLoading(true);
+  //
+  //     // API 함수 호출
+  //     const data = await generateDockerfile(dockerConfig);
+  //     console.log('호출 직후 data: ', data);
+  //
+  //     // 응답 결과 설정
+  //     // setResult(data);
+  //     console.log('setResult 직후 data: ', data);
+  //
+  //     navigate('/new/configure');
+  //   } catch (error) {
+  //     alert('Dockerfile 생성 중 오류 발생');
+  //     console.error('오류 메시지: ', error);
+  //   } finally {
+  //     // setIsLoading(false);
+  //   }
+  // };
 
   return (
     <div className="">
