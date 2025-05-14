1219e7395c239e3f010dd50734a9d5e41742b915
유준선 <rjs7289@naver.com>
2025-05-14 17:36:35 +0900

    chore: 배럴 import 추가
    - LoginPage, InitialPage 추가
    
    S12P31A301-134

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,3 +3,5 @@ export { default as NewBuildPage } from './newBuildPage';
 export { default as DockerSettings } from './DockerSettingsPage';
 export { default as ConfigurePage } from './ConfigurePage';
 export { default as BuildInfoPage } from './BuildInfoPage.tsx';
+export { default as LoginPage } from './BuildInfoPage.tsx';
+export { default as InitialPage } from './BuildInfoPage.tsx';
