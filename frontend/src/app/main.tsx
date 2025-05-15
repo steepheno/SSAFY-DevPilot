c145f052183d0b8ff2bec70c04c38e0fedb2d31e
isemae <deleted.lee13@gmail.com>
2025-05-15 12:56:58 +0900

    feat: msw 환경설정[이지운]

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,6 +1,25 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import Router from '@/app/routes';
 import './styles/index.css';
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={Router} />);
+async function enableMocking() {
+  if (import.meta.env.MODE !== 'dev') {
+    return;
+  }
+
+  const { worker } = await import('../mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <RouterProvider router={Router} />
+    </StrictMode>,
+  );
+});
