5c9e73befcd6ffda44532fa8ac58693ab47b7b65
isemae <deleted.lee13@gmail.com>
2025-05-19 15:17:27 +0900

    chore: dev debug를 위한 msw 주석처리[이지운]

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,25 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import Router from '@/app/App';
-import './styles/index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import Router from "@/app/App";
+import "./styles/index.css";
 
 async function enableMocking() {
-  if (import.meta.env.MODE !== 'dev') {
+  if (import.meta.env.MODE !== "dev") {
     return;
   }
 
-  const { worker } = await import('../mocks/browser');
-
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
+  const { worker } = await import("../mocks/browser");
   return worker.start();
 }
 
-enableMocking().then(() => {
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <RouterProvider router={Router} />
-    </StrictMode>,
-  );
-});
+// enableMocking().then(() => {
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={Router} />
+  </StrictMode>,
+);
+// });
