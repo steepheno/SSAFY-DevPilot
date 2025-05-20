7ff51ec24926574740b5ed639ad8dd37a131fd2f
isemae <deleted.lee13@gmail.com>
2025-05-21 02:51:24 +0900

    chore: 로그 관련 페이지 내 표시 방식 변경[이지운]

--- frontend/src/pages/buildLog/ui/BuildDetail.tsx
+++ frontend/src/pages/buildLog/ui/BuildDetail.tsx
@@ -4,6 +4,8 @@ import DetailButton from '@/shared/ui/DetailButton.tsx';
 import DetailInput from '@/shared/ui/DetailInput.tsx';
 import { useJobs } from '@/features/jobs/model/useJobs';
 import { formatTimestamp } from '@/shared/lib/time';
+import { instance } from '@/shared/api/instance';
+import { getBuildStream } from '@/features/jobs/api';
 
 const BuildDetail = () => {
   const [isEditMode, setIsEditMode] = useState(false);
@@ -15,11 +17,19 @@ const BuildDetail = () => {
     buildId: string;
   }>();
   const { build, isBuildError, isBuildLoading } = useJobs(jobName!, buildId!);
+  const [streamLines, setStreamLines] = useState<string[]>([]);
 
   useEffect(() => {
-    console.log(buildId);
-    console.log(build);
-  }, [buildId, build]);
+    getBuildStream(jobName!, buildId!)
+      .then((data) => {
+        const lines = data
+          .split('\n')
+          .filter((line: any) => line.startsWith('data:'))
+          .map((line: any) => line.replace(/^data:\s*/, '').trim());
+        setStreamLines(lines);
+      })
+      .catch((err) => console.error(err));
+  }, [buildId]);
 
   const submitDetailContents = () => {
     setSavedText(text);
@@ -28,7 +38,7 @@ const BuildDetail = () => {
 
   return (
     <>
-      <div className="my-10 mr-5">
+      <div className="my-5 mr-5">
         {isBuildLoading ? (
           <p>로딩 중...</p>
         ) : (
@@ -62,6 +72,16 @@ const BuildDetail = () => {
                 setIsEditMode={setIsEditMode}
                 onSubmit={submitDetailContents}
               />
+            </div>
+
+            <div className="h-[60vh] max-h-full overflow-y-auto whitespace-pre-wrap bg-gray-200 p-2 font-mono text-sm">
+              {streamLines.map((line, idx) => (
+                <div key={idx} className="flex items-start">
+                  <div className="w-10 select-none pr-4 text-right text-gray-400">{idx + 1}</div>
+                  <div className="mx-2" />
+                  <div className="flex-1 break-words">{line}</div>
+                </div>
+              ))}
             </div>
           </>
         )}
