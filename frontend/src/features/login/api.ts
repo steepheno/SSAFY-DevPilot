01ae23fc764328e9817f85504c3e659097c1d9ee
유준선 <rjs7289@naver.com>
2025-05-21 10:03:08 +0900

    fix: src/features/login/api.ts 충돌 해결[유준선]
    - frontend-develop 병합 충돌 해결
    
    S12P31A301-104

--- frontend/src/features/login/api.ts
+++ frontend/src/features/login/api.ts
@@ -2,10 +2,6 @@ import { instance } from '@/shared/api/instance';
 import { JenkinsLogin } from '@/features/login/types';
 
 export const loginJenkins = async (password: JenkinsLogin): Promise<boolean> => {
-<<<<<<< HEAD
-  const response = await instance.post('/jenkinsapi/login', password);
-  return response.status === 200;
-=======
   try {
     const response = await instance.post('/jenkinsapi/login', password);
     return response.status === 200;
@@ -13,5 +9,4 @@ export const loginJenkins = async (password: JenkinsLogin): Promise<boolean> => 
     console.error('API 통신 중 에러 발생: ', error);
     return false;
   }
->>>>>>> 9a403fbb37117d760cfeb6a1c5653e3fa5e1e482
 };
