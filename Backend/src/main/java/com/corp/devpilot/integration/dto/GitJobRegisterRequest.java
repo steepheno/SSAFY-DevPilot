f827f1b87bcb7e34cb712aae056385831b9c6361
JaeseoHan <hanahyun1@naver.com>
2025-05-22 12:42:36 +0900

    refactor: 테스트[한재서]

--- Backend/src/main/java/com/corp/devpilot/integration/dto/GitJobRegisterRequest.java
+++ Backend/src/main/java/com/corp/devpilot/integration/dto/GitJobRegisterRequest.java
@@ -13,8 +13,8 @@ public class GitJobRegisterRequest {
 	private String gitCredentialsId;    // Jenkins에 등록할 자격증명 ID
 	private String gitPersonalToken;
 	private String gitPersonalCredentialsId;
-	private String gitUsername;
+	private String gitUsername = "hanahyun1";
 	private String gitRepoUrl;          // Git 저장소 URL
-	private String jenkinsJobName;      // Jenkins Job 이름
+	private String jenkinsJobName = "damul";      // Jenkins Job 이름
 
 }
